8b74d5185b152b7daf6d3a08c31babcb6c233306
Tommy Hodgins <tomhodgins@gmail.com>
2019-07-08 00:43:15 -0400

    Annotate plugin and add tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # element-match-media
 
-**Like window.matchMedia() but for HTML elements.**
+**Like [window.matchMedia()](https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia) but for HTML elements.**
 
 ## About
 
@@ -9,5 +9,12 @@ Parse media queries from the context of an element.
 ## Usage
 
 ```js
-elementMatchMedia()
+elementMatchMedia(element, mediaQueryString)
 ```
+
+- `element` is a reference to a DOM node, or a string representing a CSS selector to match with `document.querySelector()`
+- `mediaQueryString` is a string containing a media query
+
+This function evaluates the `mediaQueryString` as though the viewport's dimensions matched those of the element given to the function. It returns the [`MediaQueryList`](https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList) object.
+
+Most of the time you use this you'll probably be wanting to check the `.matches` property to see whether the media query evaluates to `true` or `false`.
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 export default function(
-  element = '',
-  mediaQueryString = ''
+  element = document.documentElement,
+  mediaQueryString = 'screen'
 ) {
+
+  // Element is either a reference to a DOM node, or a CSS selector
   element = element instanceof Element
     ? element
     : document.querySelector(element)
+
+  // Look for <iframe id=match-media>…
   let iframe = document.querySelector('iframe#match-media')
+
+  // If it doesn't exist, create it and attach it to the document
   if (iframe === null) {
     iframe = document.createElement('iframe')
     iframe.id = 'match-media'
     iframe.style.cssText = `
       border: none;
       position: fixed;
-      right: -100vw;
-      bottom: -100vh;
+      translate(-100vw, -100vh);
     `
-    document.body.appendChild(iframe)
+    document.documentElement.appendChild(iframe)
   }
-  ['Width', 'Height'].forEach(dimension =>
-    iframe[dimension.toLowerCase()] = element[`offset${dimension}`]
+
+  // Check iframe dimensions
+  const dimensions = element.getBoundingClientRect()
+
+  // For both width & height, make iframe match element size
+  ;['width', 'height'].forEach(dimension =>
+    iframe[dimension] = dimensions[dimension]
   )
+
+  // Why do we have to trigger something here?
+  iframe.offsetWidth
+
+  // Return result of window.matchMedia() from the window inside the iframe
   return iframe.contentWindow.matchMedia(mediaQueryString)
 }
